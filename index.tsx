8f19e6fad90ba7f773a8f912650e2833fef637ef
f1shy-dev <56125930+f1shy-dev@users.noreply.github.com>
2024-08-08 19:53:42 +0100

    remove codeblock rule

--- index.tsx
+++ index.tsx
@@ -1330,33 +1330,33 @@ export function compiler(
       },
     },
 
-    [RuleType.codeBlock]: {
-      match: blockRegex(CODE_BLOCK_R),
-      order: Priority.MAX,
-      parse(capture /*, parse, state*/) {
-        return {
-          lang: undefined,
-          text: capture[0].replace(/^ {4}/gm, '').replace(/\n+$/, ''),
-        }
-      },
+    // [RuleType.codeBlock]: {
+    //   match: blockRegex(CODE_BLOCK_R),
+    //   order: Priority.MAX,
+    //   parse(capture /*, parse, state*/) {
+    //     return {
+    //       lang: undefined,
+    //       text: capture[0].replace(/^ {4}/gm, '').replace(/\n+$/, ''),
+    //     }
+    //   },
 
-      render(node, output, state) {
-        return (
-          <pre key={state.key}>
-            <code
-              {...node.attrs}
-              className={node.lang ? `lang-${node.lang}` : ''}
-            >
-              {node.text}
-            </code>
-          </pre>
-        )
-      },
-    } as MarkdownToJSX.Rule<{
-      attrs?: ReturnType<typeof attrStringToMap>
-      lang?: string
-      text: string
-    }>,
+    //   render(node, output, state) {
+    //     return (
+    //       <pre key={state.key}>
+    //         <code
+    //           {...node.attrs}
+    //           className={node.lang ? `lang-${node.lang}` : ''}
+    //         >
+    //           {node.text}
+    //         </code>
+    //       </pre>
+    //     )
+    //   },
+    // } as MarkdownToJSX.Rule<{
+    //   attrs?: ReturnType<typeof attrStringToMap>
+    //   lang?: string
+    //   text: string
+    // }>,
 
     [RuleType.codeFenced]: {
       match: blockRegex(CODE_BLOCK_FENCED_R),
